63c5e5a3d17f299fcd724d07012e3486aa1e61ad
周诚 <zhoucheng133@outlook.com>
2023-07-18 16:23:28 +0800

    完成基本框架

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
 	<div class="body" :style="{'width':bodyWidth+'px'}">
-		1212
+		<table>
+			<tr>
+				<td>高度格子数</td>
+				<td><a-input-number v-model:value="inputHight" :min="1" /></td>
+			</tr>
+			<tr>
+				<td>宽度格子数</td>
+				<td><a-input-number v-model:value="inputWidth" :min="1" /></td>
+			</tr>
+		</table>
+		<a-divider />
+		<table>
+			<tr v-for="row in inputHight" :key="row">
+				<td v-for="col in inputWidth" :key="col" @click="cellClick(row,col)" :class="isSelected(row,col)==true?'tableSelected':'tableShown'">
+					{{ row }}:{{ col }}
+				</td>
+			</tr>
+		</table>
+		<a-button type="link" style="margin-top: 30px;" @click="clear">清空</a-button>
 	</div>
 </template>
 
@@ -9,9 +27,57 @@ export default {
 	data() {
 		return {
 			bodyWidth:1000,
+
+			inputHight:1,
+			inputWidth:1,
+
+			firstClick:null,
+			secondClick:null
 		}
 	},
 	methods: {
+		clear(){
+			this.firstClick=null;
+			this.secondClick=null;
+		},
+		isBetween(num1,num2,target){
+			if (num1 > num2) {
+				[num1, num2] = [num2, num1];
+			}
+			return target >= num1 && target <= num2;
+		},
+		isSelected(row,col){
+			if(this.firstClick==null && this.secondClick==null){
+				return false;
+			}else if(this.firstClick!=null && this.secondClick==null){
+				if(row==this.firstClick.row && col==this.firstClick.col){
+					return true;
+				}else{
+					return false;
+				}
+			}else if(this.firstClick!=null && this.secondClick!=null){
+				if(this.isBetween(this.firstClick.row,this.secondClick.row,row) && this.isBetween(this.firstClick.col,this.secondClick.col,col)){
+					return true;
+				}else{
+					return false;
+				}
+			}
+			return false;
+		},
+		cellClick(row,col){
+			if(this.firstClick==null){
+				this.firstClick={
+					row: row,
+					col: col,
+				}
+				console.log(this.firstClick);
+			}else if(this.firstClick!=null && this.secondClick==null){
+				this.secondClick={
+					row: row,
+					col: col,
+				}
+			}
+		},
 		pageAdapt(){
 			var width=window.innerWidth;
 			if(width<1000){
@@ -34,9 +100,34 @@ export default {
 </script>
 
 <style scoped>
+.tableSelected{
+	background-color: red;
+	padding-bottom: 10px;
+	user-select: none;
+	color: white;
+	border: 3px solid white;
+}
+.tableShown:hover{
+	background-color: rgb(207, 135, 0);
+	cursor: pointer;
+}
+.tableShown{
+	background-color: orange;
+	padding-bottom: 10px;
+	user-select: none;
+	color: white;
+	border: 3px solid white;
+	transition: all ease-in-out .3s;
+}
+td{
+	padding-right: 10px;
+	padding-left: 10px;
+	padding-top: 10px;
+}
 .body{
-	/* width: 1000px; */
-	background-color: rgb(216, 216, 216);
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 }
 </style>
 
