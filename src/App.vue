e7bc1bc142b3b84f179faf0099b2dddd0ac7c442
周诚 <zhoucheng133@outlook.com>
2023-07-18 15:44:28 +0800

    自适应屏幕

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-	<div>
-		<a-button>按钮</a-button>
+	<div class="body" :style="{'width':bodyWidth+'px'}">
+		1212
 	</div>
 </template>
 
@@ -8,22 +8,49 @@
 export default {
 	data() {
 		return {
-			
+			bodyWidth:1000,
 		}
 	},
 	methods: {
+		pageAdapt(){
+			var width=window.innerWidth;
+			if(width<1000){
+				this.bodyWidth=width;
+			}else{
+				this.bodyWidth=1000;
+			}
+		}
+	},
+	created() {
 		
 	},
+	mounted(){
+		this.pageAdapt();
+		window.onresize=()=>{
+			this.pageAdapt();
+		}
+	}
 }
 </script>
 
+<style scoped>
+.body{
+	/* width: 1000px; */
+	background-color: rgb(216, 216, 216);
+}
+</style>
+
 <style>
+body{
+	display: flex;
+	justify-content: center;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 40px;
 }
 </style>
